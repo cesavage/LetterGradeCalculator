2b9214686fd56a167b5a5a570e0474ae68f0217c
Chris Savage <chris@christophersavage.com>
2018-05-21 20:01:50 -0400

    Initial commit

---
+++ app/src/main/java/com/christophersavage/lettergradecalculator/GradeCalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.christophersavage.lettergradecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GradeCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade_calculator);
+    }
+}
